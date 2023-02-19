51668e07d23b1998c6af3a8e7e9bb50740c3b790
CarolinaHumanez <124937617+CarolinaHumanez@users.noreply.github.com>
2023-02-19 16:03:12 -0500

    Versión 1

---
+++ src/main/java/Auto.java
@@ -0,0 +1,41 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	String marca;
+	Asiento asientos[];
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+
+	int cantidadAsientos() {
+		int numAsientos = 0;
+
+		for (int i = 0; i < asientos.length; i++) {
+			if (asientos[i] instanceof Asiento) {
+				numAsientos++;
+			}
+		}
+		return numAsientos;
+	}
+
+	String verificarIntegridad() {
+		if (registro == motor.registro) {
+
+			for (int i = 0; i < asientos.length; i++) {
+
+				if (asientos[i] != null) {
+
+					if (asientos[i].registro != registro) {
+						return "las piezas no son originales";
+					}
+				}
+			}
+			return "Auto original";
+		} else {
+			return "Las piezas no son originales";
+		}
+	}
+
+}
